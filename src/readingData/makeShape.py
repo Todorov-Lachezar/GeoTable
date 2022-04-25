ae51e356407cc141259c7df755a7935c4fab69d4
Lachezar Todorov <lxt9427@g.rit.edu>
2022-04-24 20:04:01 -0400

    Cleaned up the code and Updated README
    Cleaned up the code by removing lavaflow, unused imports and variables, and irrelevant comments. Updated the README to include more up to date information.

--- src/readingData/makeShape.py
+++ src/readingData/makeShape.py
@@ -1,23 +1,15 @@
-#python program to demonstare working with ArcGIS Pro cloned environment in an external IDE
+#python program to demonstrate working with ArcGIS Pro cloned environment in an external IDE
 #The program will:
-# (1) read conflict CSV data for AFG from HDX, 
-# (2) parse the CSV contents into a shapefile, and
-# (3) render a simple bar chart of event fatalities and dates
+# (1) read PDF data, 
+# (2) parse the table data it finds
+# (3) create a shapefile on the data that it finds
 #
-#Brian Tomaszewski - bmtski@rit.edu
-#22 September 2021
-#C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3 - default ArcGIS Pro Python
-#C:\Users\*your name*\AppData\Local\ESRI\conda\envs\arcgispro-py3-clone - clone of default ArcGIS Pro Python
+#Lachezar Todorov - lxt9427@rit.edu
+#24 April 2022
 
 #import statments - use Conda/ArcGIS Pro Python manager 
 import arcpy
-import requests
-import csv
 import sys
-import pandas
-#will need to install plotly in your python environment
-#import plotly
-#import plotly.graph_objects as go
 
 from read import reading
 
@@ -33,11 +25,6 @@ SHAPEFILE_FIELD_ID = "Id" #automatically added when the shapefile is created, no
 SHAPEFILE_FIELD_EVENT_NAME = "name"
 SHAPEFILE_FIELD_EVENT_TYPE = "cases"
 SHAPEFILE_FIELD_EVENT_PERCENTAGE = "percent"
-
-#AFG conflict CSV data from HDX
-#https://data.humdata.org/dataset/ucdp-data-for-afghanistan
-#MAIN_CSV_URL = r'https://data.humdata.org/dataset/a5c24c29-5137-411c-ac08-523d8eda5153/resource/8b486c45-bf57-4f40-8816-8b1d91863c91/download/conflict_data_afg.csv'
-#https://www.pcr.uu.se/research/ucdp/methodology/
 
 ## define the shapefile that will be created from the csv data ##
 #the directory where various files will be written to, modify for the system this code will run on
@@ -61,28 +48,13 @@ arcpy.AddField_management(Output_Shapefile_FeatureClass, SHAPEFILE_FIELD_EVENT_N
 arcpy.AddField_management(Output_Shapefile_FeatureClass, SHAPEFILE_FIELD_EVENT_TYPE, "TEXT", field_length=255)
 arcpy.AddField_management(Output_Shapefile_FeatureClass, SHAPEFILE_FIELD_EVENT_PERCENTAGE, "TEXT", field_length=255)
 
-# #open CSV file using requests
-# #https://requests.readthedocs.io/en/master/
-# req = requests.get(MAIN_CSV_URL)
-# csv_content = req.content
 
-# #convert the web content to a string
-# csv_in_fixed = str(csv_content, 'utf-8')
-# #split the string into lines so it can be parsed as CSV
-# #https://www.w3schools.com/python/ref_string_splitlines.asp
-# lines = csv_in_fixed.splitlines()
-
-# #put the CSV into a dictionary
-# datareader = csv.DictReader(lines)
+# get the dictionary from the read.py class
 datareader = reading.getDictionary()
 
 #identifier for each record
 rowidval = 0
 
-#lists that will go into the plotly bar chart
-# event_dates = []
-# event_fatalities = []
-# event_location = []
 #loop through the contents of the CSV and put the  it into a shapefile
 nameArray = datareader[TABLE_FIELD_NAME]
 latitudeArray = datareader[TABLE_FIELD_LATITUDE]
@@ -107,12 +79,6 @@ for x in range(len(nameArray)):
         
         cursor.insertRow((rowidval,nameArray[x],casesArray[x],percentArray[x],xy))
         
-        #add dates for the plot if 1 or more fatalities were found, dates and fatalities will be plotted with ploty
-        # if (int(row[CSV_FIELD_EVENT_FATALITIES])) >= 1:
-        #     event_dates.append(row[CSV_FIELD_EVENT_DATE])
-        #     event_fatalities.append(int(row[CSV_FIELD_EVENT_FATALITIES]))
-        #     event_location.append(row[CSV_FIELD_EVENT_LOCATION]) 
-        
         
     except Exception:
         e = sys.exc_info()[1]
@@ -121,17 +87,5 @@ for x in range(len(nameArray)):
         rowidval -= 1
 
     del cursor
-    
-#make a simple chart from the CSV data
-#https://plotly.com/python/creating-and-updating-figures/
-
-""" fig = go.Figure(
-    data=[go.Bar(x=event_dates, y=event_fatalities)],
-    layout=go.Layout(
-        title=go.layout.Title(text="AFG Conflict Dates and Fatalities")
-    )
-)
-
-fig.write_html(ROOT_DIRECTORY + 'dates_and_fatalities.html', auto_open=True) """
 
 print("finished")
--- src/readingData/read.py
+++ src/readingData/read.py
@@ -1,30 +1,10 @@
+#Note: Antivirus applications, such as Avast, seem to consider the 
+# camlot library as a potential threat. Make sure to label python.exe 
+# as an exluded file.
 import camelot
 
-import urllib
-#from urllib.request import urlopen
-
-#from twill import browser
 from requests import Session
-from bs4 import BeautifulSoup as bs
 
-#import fiona
-
-"""
-#Ask how many pages to read
-print("How many pages do you want to read?")
-numOfPages = input()
-
-Read the pdf file using the camelot library
-pageNum = "1"
-if int(numOfPages) > 1:
-    for x in range(int(numOfPages)+1):
-        if x == 0 or x == 1:
-            continue
-        else:
-            pageNum = pageNum + "," + str(x)
-print(pageNum)
-tables = camelot.read_pdf(file, pages = pageNum)
-"""
 class reading():
     def getTable(file):
         #Ask what page to read
@@ -63,8 +43,7 @@ class reading():
 
     def getGeoNamesData(tables, foundTable, foundColumn, state):
         #Have the code login into the http://api.geonames.org/ website 
-        #as "agentp" so they can use the service 
-        numOfData = input("Enter the number of different sets of data")
+        # as "agentp" so they can use the service 
 
         dictionary = {"Name":[], "Latitude":[], "Longitude":[], "Cases":[], "Percent":[]}
         with Session() as s:
@@ -76,33 +55,22 @@ class reading():
                     if c > 0:
                         county = tables[foundTable[t]].df[foundColumn[t]][c]
                         home_page = s.get("http://api.geonames.org/geocodeJSON?q={}+{}&username=agentp".format(county, state))
-                        # print("\n\n")
                         latIndex = str(home_page.content).find("lat")
                         longIndex = str(home_page.content).find("lng")
                         adminNameIndex = str(home_page.content).find("adminName2")
-                        # print(home_page.content)
                         lat = reading.findValues(str(home_page.content), latIndex, len("lat"))
                         long = reading.findValues(str(home_page.content), longIndex, len("long"))
                         name = reading.findValues(str(home_page.content), adminNameIndex, len("adminName2"))
                         cases = tables[foundTable[t]].df[foundColumn[t]+1][c]
                         percent = tables[foundTable[t]].df[foundColumn[t]+2][c]
-                        # print(lat)
-                        # print(long)
-                        # print(name)
-                        # print(cases)
-                        # print(percent)
+
+
                         dictionary["Name"].append(name)
                         dictionary["Latitude"].append(lat)
                         dictionary["Longitude"].append(long)
                         dictionary["Cases"].append(cases)
                         dictionary["Percent"].append(percent)
         return dictionary
-
-                        
-
-
-
-    #link = "http://api.geonames.org/geocodeJSON?q=...&username=agentp"
 
     def getDictionary():
         #PDF file
@@ -121,19 +89,17 @@ class reading():
                     foundColumn.append(y)
 
         #tables[x].df[y][z] where 'x' represents which table to look at (if 
-        #multiple tables were found), 'y' represents the column of the table
-        #in question, and 'z' represents the row of the table.
+        # multiple tables were found), 'y' represents the column of the table
+        # in question, and 'z' represents the row of the table.
+        
         #Note: The headers of the table are commonly at index 0. Also the 
-        #header can sometimes be all in one cell making index 0 of another 
-        #column be empty.
+        # header can sometimes be all in one cell making index 0 of another 
+        # column be empty.
+        
         #Note: len() of the tables[x].df gets the number of rows in a table
-        #How do you determine the number of columns?
 
         #Convert the table to a csv file
         tableData[0].to_csv("test.csv")
-
-        #Convert the table to a excel file
-            #tables[0].to_excel("test2.xlsx")
 
         print("Enter the State")
         state = input()
